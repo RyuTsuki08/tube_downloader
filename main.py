76f9395a41a7041df05b492459a66d4f207c35e5
Tomas <79181564+RyuTsuki08@users.noreply.github.com>
2024-03-26 02:30:11 -0400

    Add 'http://localhost:5174' to Video class origins
    This commit adds a new URL 'http://localhost:5174' to the origins list in the Video class.

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ class Video:
 origins = [
     "http://localhost",
     "http://localhost:8080",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://localhost:5174"
 ]
 
 app.add_middleware(
